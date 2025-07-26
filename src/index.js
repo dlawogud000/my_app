67521a0a73db0a995afaea5459dbd3cb922189e5
dlawogud000 <jh020826@gmail.com>
2025-07-26 23:10:07 +0900

    tic_tac_toe game

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import TicTacToe from './Tic_Tac_Toe/Tic_Tac_Toe.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(<App/>);  
 
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <TicTacToe />
   </React.StrictMode>
 );
 
